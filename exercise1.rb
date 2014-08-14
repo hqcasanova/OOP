27f1f623434a396a73cf4bfc69ac591e36ce2f0e
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-14 13:15:38 -0400

    Hierarchy Person-whatever set up

--- exercise1.rb
+++ exercise1.rb
@@ -1,12 +1,16 @@
-class Student
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Student < Person
   def learn
     "I get it!"
   end
-
-
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object"
   end
