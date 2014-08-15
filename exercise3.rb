bc4777a37896c4c97febb0f5869568f42e09bab8
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-15 01:10:54 -0400

    Changed apply_tax. Cart.total_taxes with bugs

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,5 @@
+#Note: class design loosely based on StackOverflow answer: http://stackoverflow.com/questions/14973770/object-oriented-design#answers
+
 ##########Representation of a tax authority with powers to set basic sales tax, import duty and to define the tax exemption list
 class Taxman
   attr_accessor :sales_tax, :import_duty, :exempt_basic
@@ -36,11 +38,12 @@ class Item
     unless taxman.exempt_basic.include?(@type)
       @taxes += taxman.sales_tax
     end
+    @price += (@price * @taxes / 5.0).round / 20.0
   end
 
-  #Full description of the item's state, with price including taxes
+  #Full description of the item's state, with price including taxes ((x * 20).round / 20.0 => rounding up to nearest 0.05)
   def to_s
-    "#{@name} at #{@price + (@price * @taxes / 100.0)}"
+    "#{@name} at #{@price}"
     if @is_imported
       'imported ' + description
     end 
@@ -49,16 +52,16 @@ end
 
 ##########Shopping cart with receipt printing capabilities
 class Cart
-  attr_accessor :cart
+  attr_accessor :list
 
   def initialize
     #Hash of item-quantity pairs
-    @cart = {}
+    @list = {}
   end
 
   #Prints out the list of purchased items along with their price including taxes
   def to_s
-    @cart.each do |item, quantity|
+    @list.each do |item, quantity|
       "#{quantity} #{item}\n"
     end
   end
@@ -66,7 +69,7 @@ class Cart
   #Calculates and prints out total amount of taxes for all listed items
   def total_taxes
     total_taxes = 0;
-    @cart.each do |item, quantity|
+    @list.each do |item, quantity|
       total_taxes += item.taxes
     end
     puts "Sales taxes: #{total_taxes}"
@@ -78,3 +81,9 @@ class Cart
     total_taxes
   end 
 end
+
+ontario_tax = Taxman.new(10, 5, ['books', 'food', 'medical'])
+book = Item.new('book', '12.49', 'books', false)
+loblaws_cart = Cart.new
+loblaws_cart.list = {book => 1}
+loblaws_cart.checkout
