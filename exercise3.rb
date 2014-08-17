3743451502f053cf6953d8ff04e0927af4ee8d3e
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-17 13:12:44 -0400

    Precision fixed. Tax application with bugs

--- exercise3.rb
+++ exercise3.rb
@@ -1,19 +1,32 @@
 #Note: class design loosely based on StackOverflow answer: http://stackoverflow.com/questions/14973770/object-oriented-design#answers
 
+require 'bigdecimal'
+MONEY_FORMAT = '$%.02f'
+
 ##########Representation of a tax authority with powers to set basic sales tax, import duty and to define the tax exemption list
 class Taxman
-  attr_accessor :sales_tax, :import_duty, :exempt_basic
+  attr_accessor :sales_tax, :import_duty, :exempt_basic   #taxes change over time
+  attr_reader :name
 
-  def initialize(sales_tax, import_duty, exempt_basic)
-    @sales_tax = sales_tax
-    @import_duty = import_duty
+  def initialize(authority, sales_tax, import_duty, exempt_basic)
+    @authority = authority
+    @sales_tax = BigDecimal.new(sales_tax)
+    @import_duty = BigDecimal.new(import_duty)
     @exempt_basic = exempt_basic
+  end
+
+  #Calculate tax for a given price tag
+  def apply_tax(price, tax)
+    puts MONEY_FORMAT % price
+    puts MONEY_FORMAT % tax
+    return (price * tax / BigDecimal.new(5)).round / BigDecimal.new(20)   #rounding up to nearest 0.05
   end
 
   #Describes current tax rates and gives a list of exempt products
   def to_s
-    "Taxes: #{@sales_tax}% of basic sales tax on all goods except #{exempt_basic}.\n" + 
-    "#{@import_duty}% of import duty on all imported goods"
+    "Tax authority: #{@authority}\n" +
+    "-Sales Taxes: #{@sales_tax.to_i}% rate on all goods except #{exempt_basic}.\n" + 
+    "-Import Duty: #{@import_duty.to_i}% rate on all imported goods."
   end
 end
 
@@ -22,68 +35,132 @@ class Item
   attr_reader :name, :is_imported, :taxes
   attr_accessor :price
 
-  def initialize(name, price, type, is_imported)
+  def initialize(name, type, is_imported)
     @name = name
-    @price = price
+    @price = BigDecimal.new(0)
+    @taxes = BigDecimal.new(0)
     @type = type
     @is_imported = is_imported
   end
 
   #Calculates corresponding tax total depending on properties and current rates set by tax authority
   def apply_taxes(taxman)
-    @taxes = 0
+    @taxes = BigDecimal.new(0)
     if @is_imported
-      @taxes += taxman.import_duty
+      @taxes += taxman.apply_tax(@price, taxman.import_duty)
     end
     unless taxman.exempt_basic.include?(@type)
-      @taxes += taxman.sales_tax
+      @taxes += taxman.apply_tax(@price, taxman.sales_tax)
     end
-    @price += (@price * @taxes / 5.0).round / 20.0
+    @price += taxman.apply_tax(@price, @taxes)
   end
 
-  #Full description of the item's state, with price including taxes ((x * 20).round / 20.0 => rounding up to nearest 0.05)
+  #Full description of the item's state, with price including taxes
   def to_s
-    "#{@name} at #{@price}"
+    description = "#{@name} at #{MONEY_FORMAT % @price}"
     if @is_imported
-      'imported ' + description
-    end 
+      description = 'imported ' + description
+    end
+    return description 
   end
 end
 
 ##########Shopping cart with receipt printing capabilities
 class Cart
-  attr_accessor :list
+  attr_accessor :list   #Hash of item-quantity pairs
+  attr_reader :total    #Total price of purchased items (with/out taxes)
 
   def initialize
-    #Hash of item-quantity pairs
     @list = {}
   end
 
   #Prints out the list of purchased items along with their price including taxes
   def to_s
+    printed_list = ""
+    @total = 0
     @list.each do |item, quantity|
-      "#{quantity} #{item}\n"
+      item_total = quantity * item.price
+      @total += item_total
+      printed_list += "#{quantity} #{item}: #{MONEY_FORMAT % item_total}\n"
     end
+    return printed_list
   end
 
   #Calculates and prints out total amount of taxes for all listed items
   def total_taxes
-    total_taxes = 0;
+    @total_taxes = 0
     @list.each do |item, quantity|
-      total_taxes += item.taxes
+      @total_taxes += quantity * item.taxes
     end
-    puts "Sales taxes: #{total_taxes}"
+    puts "Sales Taxes: #{MONEY_FORMAT % @total_taxes}"
   end
 
   #Convenience method that prints out standard receipt, including subtotal of taxes
   def checkout
-    to_s
+    puts self
     total_taxes
-  end 
+    puts "Total: #{MONEY_FORMAT % @total}\n"
+  end
 end
 
-ontario_tax = Taxman.new(10, 5, ['books', 'food', 'medical'])
-book = Item.new('book', '12.49', 'books', false)
+#Sets up a tax authority and describes it
+ontario = Taxman.new('Government of Ontario', 10, 5, ['books', 'food', 'medical'])
+puts "#{ontario}\n"
+
+#Creates items and cart
+book = Item.new('book', 'books', false)
+cd = Item.new('music CD', 'music', false)
+chocbar = Item.new('chocolate bar', 'food', false)
+chocolates = Item.new('box of chocolates', 'food', true)
+perfume = Item.new('bottle of perfume', 'cosmetics', false)
+perfume_imported = Item.new('bottle of perfume', 'cosmetics', true)
+pills = Item.new('packet of headache pills', 'medical', false)
 loblaws_cart = Cart.new
-loblaws_cart.list = {book => 1}
+
+#Loblaws visit 1
+puts "\nCart 1:"
+book.price = BigDecimal.new('12.49')
+cd.price = BigDecimal.new('14.99')
+chocbar.price = BigDecimal.new('0.85')
+loblaws_cart.list = {book => 1, cd => 1, chocbar => 1}
+puts "#{loblaws_cart}"
+
+#Checkout 1
+puts "\nReceipt 1:"
+book.apply_taxes(ontario)
+cd.apply_taxes(ontario)
+chocbar.apply_taxes(ontario)
 loblaws_cart.checkout
+
+#Loblaws visit 2
+puts "\nCart 2:"
+chocolates.price = BigDecimal.new('10.00')
+perfume_imported.price = BigDecimal.new('47.50')
+loblaws_cart.list = {chocolates => 1, perfume_imported => 1}
+puts "#{loblaws_cart}"
+
+# #Checkout 2
+puts "\nReceipt 2:"
+chocolates.apply_taxes(ontario)
+perfume.apply_taxes(ontario)
+loblaws_cart.checkout
+
+# #Loblaws visit 3
+#puts "\nCart 3"
+# perfume_imported.price = 32.19
+# puts perfume_imported
+# perfume.price = 20.89
+# puts perfume
+# pills.price = 9.75
+# puts pills
+# chocolates.price = 11.85
+# puts chocolates
+# loblaws_cart.list = {perfume_imported => 1, perfume => 1, pills => 1, chocolates => 1}
+# puts "#{loblaws_cart}\n"
+
+# #Checkout 3
+# perfume_imported.apply_taxes(ontario)
+# perfume.apply_taxes(ontario)
+# pills.apply_taxes(ontario)
+# chocolates.apply_taxes(ontario)
+# loblaws_cart.checkout
